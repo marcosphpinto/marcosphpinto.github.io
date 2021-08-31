f1b492c05f92c28a37578b2660a1bf67a76b361c
mklaaa <marcosphpinto@gmail.com>
2021-08-30 21:35:44 -0300

    add my data

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,47 +7,34 @@
 import emoji from "react-easy-emoji";
 
 const illustration = {
-  animated: true // set to false to use static SVG
+  animated: false // set to false to use static SVG
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
-  subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
-  ),
-  resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing",
+  username: "Marcos Paulo",
+  title: "Hi, I'm Marcos",
+  subTitle:
+    "An Engineer with a lot of experience developing New Businesses and Products with any technology available. It can be Desktop, Web or Mobile, the goal is to deliver great products to great customers.",
+  resumeLink: "https://www.linkedin.com/in/marcospaulopinto/",
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
-  // Instagram and Twitter are also supported in the links!
+  github: "https://github.com/marcosphpinto",
+  linkedin: "https://www.linkedin.com/in/marcospaulopinto/",
+  gmail: "marcosphpinto@gmail.com",
   display: true // Set true to display this section, defaults to false
 };
 
 // Skills Section
 
 const skillsSection = {
-  title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  title: "The tools in my toolbox",
+  subTitle: "THE STUFF THAT I'VE USED UNTIL NOW",
   skills: [
-    emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
-    ),
-    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
-    emoji(
-      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
-    )
+    emoji("⚡ Building great products with the best practices in every stack")
   ],
 
   /* Make Sure to include correct Font Awesome Classname to view your icon
@@ -55,56 +42,36 @@ https://fontawesome.com/icons?d=gallery */
 
   softwareSkills: [
     {
-      skillName: "html-5",
-      fontAwesomeClassname: "fab fa-html5"
-    },
-    {
-      skillName: "css3",
-      fontAwesomeClassname: "fab fa-css3-alt"
-    },
-    {
-      skillName: "sass",
-      fontAwesomeClassname: "fab fa-sass"
-    },
-    {
-      skillName: "JavaScript",
-      fontAwesomeClassname: "fab fa-js"
-    },
-    {
-      skillName: "reactjs",
-      fontAwesomeClassname: "fab fa-react"
-    },
-    {
       skillName: "nodejs",
       fontAwesomeClassname: "fab fa-node"
-    },
-    {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
-    },
-    {
-      skillName: "npm",
-      fontAwesomeClassname: "fab fa-npm"
-    },
-    {
-      skillName: "sql-database",
-      fontAwesomeClassname: "fas fa-database"
-    },
-    {
-      skillName: "aws",
-      fontAwesomeClassname: "fab fa-aws"
-    },
-    {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
     },
     {
       skillName: "python",
       fontAwesomeClassname: "fab fa-python"
     },
     {
-      skillName: "docker",
-      fontAwesomeClassname: "fab fa-docker"
+      skillName: "sql",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "nosql",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "AWS",
+      fontAwesomeClassname: "fab fa-aws"
+    },
+    {
+      skillName: "Heroku",
+      fontAwesomeClassname: "fas fa-server"
+    },
+    {
+      skillName: "VBA",
+      fontAwesomeClassname: "fas fa-file-excel"
+    },
+    {
+      skillName: "PowerBi",
+      fontAwesomeClassname: "fas fa-chart-line"
     }
   ],
   display: true // Set false to hide this section, defaults to true
@@ -116,23 +83,10 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
-      descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      ]
-    },
-    {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+      schoolName: "Centro Universitário da FEI",
+      logo: require("./assets/images/feiLogo.png"),
+      subHeader: "Mechanical Engineering",
+      duration: "January 2012 - July 2017"
     }
   ]
 };
@@ -143,7 +97,7 @@ const techStack = {
   viewSkillBars: true, //Set it to true to show Proficiency Section
   experience: [
     {
-      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      Stack: "Product", //Insert stack or technology you have experience in
       progressPercentage: "90%" //Insert relative proficiency in percentage
     },
     {
@@ -151,8 +105,8 @@ const techStack = {
       progressPercentage: "70%"
     },
     {
-      Stack: "Programming",
-      progressPercentage: "60%"
+      Stack: "Frontend",
+      progressPercentage: "20%"
     }
   ],
   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
@@ -164,29 +118,25 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Software Engineer",
-      company: "Facebook",
-      companylogo: require("./assets/images/facebookLogo.png"),
-      date: "June 2018 – Present",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      ]
+      role: "Product Owner",
+      company: "Doc88",
+      companylogo: require("./assets/images/doc88Logo.png"),
+      date: "December 2020 – March 2021",
+      desc: "CCEE Integration"
     },
     {
-      role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "Sales - ESCO",
+      company: "Comerc Energia",
+      companylogo: require("./assets/images/comercLogo.png"),
+      date: "March 2018 - November 2020",
+      desc: "Development of Energy Efficiency Projects"
     },
     {
-      role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "Sales - Trading",
+      company: "Comerc Energia",
+      companylogo: require("./assets/images/comercLogo.png"),
+      date: "January 2016 – March 2018",
+      desc: "Energy Trading to Final Consumer"
     }
   ]
 };
@@ -196,35 +146,119 @@ To know how to get github key look at readme.md */
 
 const openSource = {
   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Some big projects you have worked on
 
 const bigProjects = {
-  title: "Big Projects",
-  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  title: "Projects",
+  subtitle: "SOME PROJECTS THAT I HELPED DEVELOP",
   projects: [
     {
-      image: require("./assets/images/saayaHealthLogo.webp"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      projectName: "O-SI",
+      projectDesc:
+        "Integration between medical devices and medical record system for a mobile health platform",
+      footerChip: [
+        {
+          name: "DynamoDB"
+        },
+        {
+          name: "NodeJs"
+        },
+        {
+          name: "AWS Lambda"
+        }
+      ],
       footerLink: [
         {
-          name: "Visit Website",
-          url: "http://saayahealth.com/"
+          name: "Company",
+          url: "https://www.o-si.com/"
         }
-        //  you can add extra buttons here.
       ]
     },
     {
-      image: require("./assets/images/nextuLogo.webp"),
-      projectName: "Nextu",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      projectName: "Contaluz",
+      projectDesc: "Development of an Energy Management App",
+      footerChip: [
+        {
+          name: "MongoDB Atlas"
+        },
+        {
+          name: "Python"
+        },
+        {
+          name: "Flask"
+        },
+        {
+          name: "Flutter"
+        },
+        {
+          name: "AWS EC2"
+        }
+      ]
+    },
+    {
+      projectName: "EYEXPLORER",
+      projectDesc: "Simulation of an Underwater Remotely Operated Vehicle",
+      footerChip: [
+        {
+          name: "Ansys Suite"
+        }
+      ]
+    },
+    {
+      projectName: "Comerc Trading - Proposal Center",
+      projectDesc: "Development of a Management and Proposal Submission System",
+      footerChip: [
+        {
+          name: "Excel"
+        },
+        {
+          name: "VBA"
+        },
+        {
+          name: "PowerBI"
+        }
+      ],
       footerLink: [
         {
-          name: "Visit Website",
-          url: "http://nextu.se/"
+          name: "Company",
+          url: "https://www.comerc.com.br/"
+        }
+      ]
+    },
+    {
+      projectName: "Aeroturbine Simulator",
+      projectDesc:
+        "Computational Simulation of an Aeroturbine's Thermodynamic Cycle",
+      footerChip: [
+        {
+          name: "EES"
+        }
+      ],
+      footerLink: [
+        {
+          name: "Article",
+          url: "https://fei.edu.br/sites/sicfei/2015/Mec%C3%A2nica/SIMULA%C3%87%C3%83O%20COMPUTACIONAL%20DO%20CICLO%20TERMODIN%C3%82MICO%20DE%20UMA%20TURBINA%20A%20G%C3%81S.pdf"
+        }
+      ]
+    },
+    {
+      projectName: "Voss - Prototype Products' Manufacture Management System",
+      projectDesc: "Coordenation between IT and stakeholders",
+      footerChip: [
+        {
+          name: "Totvs Protheus"
+        },
+        {
+          name: "SAP"
+        }
+      ],
+      footerLink: [
+        {
+          name: "Company",
+          url: "https://www.voss.com.br/"
         }
       ]
     }
@@ -287,7 +321,7 @@ const achievementSection = {
       ]
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Blogs Section
@@ -311,7 +345,7 @@ const blogSection = {
         "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Talks Sections
@@ -330,7 +364,7 @@ const talkSection = {
       event_url: "https://www.facebook.com/events/2339906106275053/"
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Podcast Section
@@ -343,22 +377,21 @@ const podcastSection = {
   podcast: [
     "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-3243454077",
-  email_address: "saadpasta70@gmail.com"
+  email_address: "marcosphpinto@gmail.com"
 };
 
 // Twitter Section
 
 const twitterDetails = {
   userName: "twitter", //Replace "twitter" with your twitter username without @
-  display: true // Set true to display this section, defaults to false
+  display: false // Set true to display this section, defaults to false
 };
 
 export {
